fd1f927043fb16c0984aad13026dbb94fdf66ea0
Philip Oesterlin <philip.oesterlin+github@gmail.com>
2023-10-26 00:17:14 +0200

    load env

--- src/lib/helpers/notification.ts
+++ src/lib/helpers/notification.ts
@@ -1,9 +1,13 @@
 import { env } from '$env/dynamic/private';
-import type { User } from '$lib/db/entities/user';
+import type { User } from '$lib/db/entities';
 import webpush from 'web-push';
 
 const publicVapidKey = env.VITE_VAPID_PUBLIC;
 const privateVapidKey = env.VAPID_PRIVATE;
+
+if (!publicVapidKey || !privateVapidKey) {
+	throw new Error('VAPID keys not set');
+}
 
 webpush.setVapidDetails('https://volleyballhtwg.netlify.app', publicVapidKey, privateVapidKey);
 
