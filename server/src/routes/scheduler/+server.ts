44358e34a6fc87bef406511c48a86a9e6a179941
Philip Oesterlin <oesterlin@ugd.ai>
2023-11-04 13:44:00 +0100

    added scheduler

--- server/src/routes/scheduler/+server.ts
+++ server/src/routes/scheduler/+server.ts
@@ -1,12 +1,16 @@
 import { Course } from "$lib/db/entities";
+import { sendNotification } from "$lib/helpers/notification";
 import type { RequestHandler } from "@sveltejs/kit";
-
-
+import { isToday } from "date-fns";
 
 export const POST: RequestHandler = async ({ locals }) => {
     const em = locals.em;
     const courses = await em.find(Course, { shouldPublish: true, notificationSent: false });
-    scheduleNotifications(courses, async (course) => {
+    for (const course of courses) {
+        if (!isToday(course.date)) {
+            continue;
+        }
+
         console.log(`Sending notifications for course ${course.id}`);
         for (const user of course.users.getItems()) {
             // TODO: add course link to notification
@@ -15,22 +19,7 @@ export const POST: RequestHandler = async ({ locals }) => {
 
         course.notificationSent = true;
         await em.persistAndFlush(course);
-
-        process.exit(0);
-    });
+    }
 
     return new Response(JSON.stringify({ message: "ok" }));
-}
-
-function scheduleNotifications(courses: Course[], callback: (course: Course) => void) {
-    courses.forEach((course) => {
-        if (!isToday(course.date)) {
-            return;
-        }
-
-        // cron job runs at 2pm every day
-        const cron = `0 14 * * *`;
-        const job = new CronJob(cron, () => callback(course));
-        job.start();
-    });
 }
