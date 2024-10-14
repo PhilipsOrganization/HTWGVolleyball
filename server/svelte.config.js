d76f3dbc68224af99d6dd5e0646c248a8d5fafe3
Philip Oesterlin <philip.oesterlin@gmail.com>
2024-10-14 23:13:35 +0200

    switch to bun

--- server/svelte.config.js
+++ server/svelte.config.js
@@ -1,13 +1,13 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from 'svelte-adapter-bun';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter(),
 		csrf: {
-			checkOrigin: false,
+			checkOrigin: false
 		}
-	},
+	}
 };
 
 export default config;
