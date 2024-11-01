8f8472e370083b76b6c3d0232d7e2fd2e4b9d3aa
Philip Oesterlin <philip.oesterlin@gmail.com>
2024-11-01 23:06:53 +0100

    allow access to courses page without login

--- server/src/routes/courses/+page.server.ts
+++ server/src/routes/courses/+page.server.ts
@@ -1,28 +1,25 @@
 import { accounts, courseSpots, courses, type Account } from '$lib/db/schema.js';
 import { serializeUser } from '$lib/helpers/account';
 import { serializeCourse } from '$lib/helpers/course';
-import { redirect } from '@sveltejs/kit';
 import { and, eq, gte, lte, sql } from 'drizzle-orm';
 import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
-	if (!locals.user) {
-		redirect(303, '/login');
-	}
-
 	const result = await locals.db
 		.select({
 			courses,
-			accountsJson: sql<Account[]>`json_agg(accounts order by ${courseSpots.createdAt} asc)`.as('accountsJson'),
+			accountsJson: sql<Account[]>`json_agg(accounts order by ${courseSpots.createdAt} asc)`.as('accountsJson')
 		})
 		.from(courseSpots)
 		.fullJoin(courses, eq(courseSpots.courseId, courses.id))
 		.fullJoin(accounts, eq(courseSpots.userId, accounts.id))
 		.groupBy(courses.id)
-		.where(and(
-			lte(courses.publishOn, sql`NOW()`),	// Only show courses that have been published
-			gte(courses.date, sql`NOW() - INTERVAL '1 day'`), // Only show courses that are 24 hours in the past
-		))
+		.where(
+			and(
+				lte(courses.publishOn, sql`NOW()`), // Only show courses that have been published
+				gte(courses.date, sql`NOW() - INTERVAL '1 day'`) // Only show courses that are 24 hours in the past
+			)
+		)
 		.orderBy(courses.date, courses.time);
 
 	const dates = new Map<string, typeof result>();
@@ -44,6 +41,6 @@ export const load: PageServerLoad = async ({ locals }) => {
 			date,
 			courses: courses.map((data) => serializeCourse(data, locals.user))
 		})),
-		user: serializeUser(locals.user)
+		user: locals.user ? serializeUser(locals.user) : undefined
 	};
 };
