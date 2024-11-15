8c286ad0fff0d2ca3d554630401394d3f9b73f63
Philip Oesterlin <philip.oesterlin+github@gmail.com>
2024-11-15 18:05:10 +0100

    style group page

--- server/src/routes/admin/groups/+page.svelte
+++ server/src/routes/admin/groups/+page.svelte
@@ -4,11 +4,6 @@
 
 <h1>Groups</h1>
 
-<form action="?/create" method="POST">
-	<input type="text" name="name" placeholder="Name" required />
-	<button type="submit">Create</button>
-</form>
-
 <ul>
 	{#each data.groups as group}
 		<li>
@@ -18,3 +13,29 @@
 		</li>
 	{/each}
 </ul>
+
+<form action="?/create" method="POST">
+	<h2>
+		Create a new group:
+	</h2>
+	<input type="text" name="name" placeholder="Name" required />
+	<button type="submit">Create</button>
+</form>
+
+<style>
+	form {
+		display: flex;
+		flex-direction: column;
+		border: 1px solid gray;
+		padding: 1rem;
+		gap: 1rem;
+	}
+
+	button {
+		background: #9cc1cf;
+		color: #000;
+		border: none;
+		padding: 0.5rem 1rem;
+		cursor: pointer;
+	}
+</style>
